1cf9b18082d4620add329d667adaed39e4de8c04
Yemini Codeur <haidara.aboubacrine11@gmail.com>
2019-08-27 17:14:15 +0000

    je modifier style et comments.

--- src/main/java/org/sid/catalogservice/dao/ProductDao.java
+++ src/main/java/org/sid/catalogservice/dao/ProductDao.java
@@ -7,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RepositoryRestResource
+@CrossOrigin("*")
 public interface ProductDao extends JpaRepository<Product, Long> {
     @RestResource(path = "/designation")
     List<Product> findByDescriptionContains(@Param("desc") String desc);
--- src/main/java/org/sid/catalogservice/entities/Category.java
+++ src/main/java/org/sid/catalogservice/entities/Category.java
@@ -1,4 +1,15 @@
 package org.sid.catalogservice.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Category {
+    @Id @GeneratedValue
+    private long id;
+    private String designation;
 }
